ea86f9effe3dc87ad479d69f5be07001d087d962
IsabelUrribarri <urribarriisabel@gmail.com>
2023-02-03 09:10:20 -0400

    trabajando en inactivar alumnos

--- academy/urls.py
+++ academy/urls.py
@@ -7,9 +7,11 @@ urlpatterns = [
     path("login", views.login_view, name="login"),    
     path("logout", views.logout_view, name="logout"),
     path("register", views.register_view, name="register"),  
-    path("asistencia", views.asistencia, name="asistencia"),  
+    path("asistencia", views.asistencia, name="asistencia"),
+    path("mantenimiento", views.mantenimiento, name="mantenimiento"),
 
     # Api Routes
     path("api/asistencias/<int:alumno_id>", views.api_asistencias, name="api_asistencias"), 
+    path("api/inactivar_alumnos", views.api_inactivar_alumnos, name="api_inactivar_alumnos"),
 ]
 
