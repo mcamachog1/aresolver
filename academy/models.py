f8fdb9fa17d3cfd8613074b70c9b982c8ea81e33
Maryvi <mcamachog@gmail.com>
2023-03-03 15:53:13 +0000

    incorporar academia adicional

--- academy/models.py
+++ academy/models.py
@@ -4,7 +4,19 @@ import datetime
 
 # Create your models here.
 class User(AbstractUser):
-    pass
+	ALUMNO = 'A'
+	REPRESENTANTE = 'R'
+	DIRECTOR = 'D'
+	TUTOR = 'T'
+	VISITANTE = 'V'
+	USER_TYPE_CHOICES = [
+		(ALUMNO, 'Alumno'),
+		(REPRESENTANTE, 'Representante'),
+		(DIRECTOR, 'Director'),
+		(TUTOR, 'Tutor'),
+		(VISITANTE, 'Visitante')
+	]
+	tipo_de_usuario = models.CharField(max_length=1, choices=USER_TYPE_CHOICES, default=VISITANTE)
 
 # Clase Tutores
 class Tutor(models.Model):
@@ -27,6 +39,18 @@ class Representante(models.Model):
 	def serialize(self):
 			return (f"{self.nombre} {self.apellido}")
 
+class Academia(models.Model):
+	nombre = models.CharField(max_length=30)
+	director = models.OneToOneField(
+		User,
+		on_delete=models.CASCADE,
+		related_name="academia",
+		null=True
+	)
+
+	def __str__(self):
+		return self.nombre
+
 class Alumno(models.Model):
 	NUEVO = 'N'
 	ACTIVO = 'A'
@@ -43,17 +67,27 @@ class Alumno(models.Model):
 	status = models.CharField(max_length=1, choices=STATUS_CHOICES, default=NUEVO)
 	fecha_creacion = models.DateTimeField(auto_now_add=True)
 	fecha_cambio = models.DateTimeField(auto_now=True)
-	representante = models.ForeignKey(Representante, on_delete=models.SET_NULL, related_name="representados", null=True) 
+	representante = models.ForeignKey(Representante, on_delete=models.SET_NULL, related_name="representados", null=True)
+	academias = models.ManyToManyField(Academia)
+
+	class Meta:
+		ordering = ['nombre']
+
+	def __str__(self):
+		return self.nombre
 
 	def serialize(self):
-			return (f"{self.nombre} {self.apellido} {self.representante.nombre}")
+		return (f"{self.nombre} {self.apellido} {self.representante.nombre}")
+
 
 class Asistencia(models.Model):
-    fecha = models.DateField(null=False, default=datetime.datetime.now())
-    alumno = models.ForeignKey(Alumno, on_delete=models.CASCADE, related_name="asistencias", null=False) 
+	fecha = models.DateField(null=False, default=datetime.datetime.now())
+	alumno = models.ForeignKey(Alumno, on_delete=models.CASCADE, related_name="asistencias", null=False) 
+	academia = models.ForeignKey(Academia, on_delete=models.CASCADE, related_name="academias", null=True)
+	tutor = models.ForeignKey(Tutor, on_delete=models.CASCADE, related_name="tutores", null=True)
 
-    def serialize(self):
-        return (f"id: {self.id} fecha: {self.fecha} - alumno: {self.alumno.nombre}")
+	def serialize(self):
+		return (f"id: {self.id} fecha: {self.fecha} - alumno: {self.alumno.nombre} - academia: {self.academia.nombre} - tutor: {self.tutor.nombre}")
 
 class Pago(models.Model):
 	fecha_pago = models.DateField()
